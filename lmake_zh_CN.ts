93b42a156f0dbe0eac16fb08905d0500c4858b31
WAAutoMaton <a547673135@gmail.com>
2017-06-01 16:42:11 +0800

    更新翻译

--- lmake_zh_CN.ts
+++ lmake_zh_CN.ts
@@ -4,64 +4,77 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="configreader.cpp" line="30"/>
+        <location filename="configreader.cpp" line="70"/>
         <source>Unexpected end.</source>
-        <translation type="unfinished">未期望的结尾。</translation>
+        <translation>未期望的结尾。</translation>
     </message>
     <message>
-        <location filename="configreader.cpp" line="32"/>
+        <location filename="configreader.cpp" line="72"/>
         <source>Unlawful variable name</source>
-        <translation type="unfinished">不合法的变量名</translation>
+        <translation>不合法的变量名</translation>
     </message>
     <message>
-        <location filename="configreader.cpp" line="57"/>
+        <location filename="configreader.cpp" line="99"/>
+        <source>too many value after operator ++</source>
+        <translation>++运算符后不允许有值</translation>
+    </message>
+    <message>
+        <location filename="configreader.cpp" line="104"/>
+        <source>too many value after operator --</source>
+        <translation>--运算符后不允许有值</translation>
+    </message>
+    <message>
+        <location filename="configreader.cpp" line="107"/>
         <source>Unknown operator %1</source>
-        <translation type="unfinished">未知操作符%1</translation>
+        <translation>未知操作符%1</translation>
     </message>
     <message>
-        <location filename="exception.cpp" line="17"/>
         <source>At file %1,</source>
-        <translation type="unfinished">在文件%1中，</translation>
+        <translation type="obsolete">在文件%1中，</translation>
     </message>
     <message>
-        <location filename="exception.cpp" line="18"/>
         <source>At line %1,</source>
-        <translation type="unfinished">第%1行，</translation>
+        <translation type="obsolete">第%1行，</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="85"/>
+        <location filename="main.cpp" line="83"/>
         <source>too many source files</source>
-        <translation type="unfinished">源文件过多</translation>
+        <translation>源文件过多</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="88"/>
+        <location filename="main.cpp" line="86"/>
         <source>no source file</source>
-        <translation type="unfinished">找不到源文件</translation>
+        <translation>找不到源文件</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="150"/>
+        <location filename="main.cpp" line="114"/>
+        <source>No such file</source>
+        <translation>找不到文件</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="149"/>
         <source>Unknown Error!</source>
-        <translation type="unfinished">未知错误！</translation>
+        <translation>未知错误！</translation>
     </message>
     <message>
-        <location filename="makefilewritter.cpp" line="50"/>
+        <location filename="makefilewritter.cpp" line="123"/>
         <source>Unknown value %2 in variable %1</source>
-        <translation type="unfinished">在变量%1中发现未知的值%2</translation>
+        <translation>在变量%1中发现未知的值%2</translation>
     </message>
     <message>
-        <location filename="makefilewritter.cpp" line="57"/>
+        <location filename="makefilewritter.cpp" line="138"/>
         <source>varibale %1 not foud</source>
-        <translation type="unfinished">找不到变量%1</translation>
+        <translation>找不到变量%1</translation>
     </message>
     <message>
-        <location filename="makefilewritter.cpp" line="59"/>
+        <location filename="makefilewritter.cpp" line="140"/>
         <source>too much value in varibale %1</source>
-        <translation type="unfinished">变量%1中的值过多</translation>
+        <translation>变量%1中的值过多</translation>
     </message>
     <message>
-        <location filename="makefilewritter.cpp" line="60"/>
+        <location filename="makefilewritter.cpp" line="141"/>
         <source>too few value in varibale %1</source>
-        <translation type="unfinished">变量%1中的值过少</translation>
+        <translation>变量%1中的值过少</translation>
     </message>
 </context>
 </TS>
